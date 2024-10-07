d41f3e290563bf2a40e681e7f9aa1ff7e14049ab
Angelo Pengue <apengue1@gmail.com>
2024-10-07 19:50:36 -0400

    Adds git org/repo to tmux status line

---
+++ .tmux/theme/gitrepo.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+pane_current_path=$1
+git_remote_url=$(git -C $pane_current_path remote get-url origin)
+
+if [ -n "$(echo $git_remote_url | grep 'http')" ]; then
+		# http(s) git remote
+		org_repo=$(echo $git_remote_url | awk -F/ '{print $(NF-1) FS $(NF)}')
+else
+		# ssh git remote
+		org_repo=$(echo $git_remote_url | awk -F: '{ print $NF }')
+fi
+
+echo $org_repo | sed 's/.git//'
