e0be9fec418443363ae3303853bdebcc41b11fe3
Angelo Pengue <7028473+APengue1@users.noreply.github.com>
2025-10-03 12:59:39 -0400

    Add nixpkgs install script (#1)
    * Initial nix commit
    
    * Loads nix after .localrc
    
    Must take precedence over any other package managers, for example brew.

---
+++ nix-install.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+channel="nixpkgs"
+
+# Remove default channel (usually unstable) and replace it with our stable version.
+nix-channel --remove $channel
+nix-channel --add https://nixos.org/channels/nixos-24.11 $channel
+nix-channel --update
+
+# Install our desired packages.
+packages=(
+        nix
+        ripgrep
+        fzf
+        tmux
+        git
+        gitmux
+        neovim
+)
+
+for package in ${packages[@]}
+do
+    nix-env -iA "$channel.$package"
+done
